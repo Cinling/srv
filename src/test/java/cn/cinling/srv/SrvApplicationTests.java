47f8eff782738332263e931a553cb3a13df6fc19
cinling <1105412681@qq.com>
2018-04-20 21:50:50 +0800

    初始化 springboot 项目

---
+++ src/test/java/cn/cinling/srv/SrvApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cinling.srv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrvApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
