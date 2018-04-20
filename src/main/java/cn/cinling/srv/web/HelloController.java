2f30518a38f99699005bc102d3be3b6ad0d11638
cinling <1105412681@qq.com>
2018-04-21 00:28:00 +0800

    添加测试页面的首页

---
+++ src/main/java/cn/cinling/srv/web/HelloController.java
@@ -0,0 +1,21 @@
+package cn.cinling.srv.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String Hello() {
+        return "hello world";
+    }
+
+    @RequestMapping("/")
+    public String Index() {
+        return "index";
+    }
+}
