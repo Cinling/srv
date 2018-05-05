e0de1a213ef09d1cf4ca990851921b845be1500c
yingzhimei_grz <281093509@qq.com>
2018-05-05 10:12:15 +0800

    添加 liquibase 数据库版本管理模块

---
+++ src/main/java/cn/cinling/srv/config/LiquibaseConfig.java
@@ -0,0 +1,23 @@
+package cn.cinling.srv.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        liquibase.setContexts("development,test,production");
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+
+}
